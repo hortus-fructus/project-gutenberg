5960240d428180967e24b2a25bff903e138d2ca7
beckclandras <clandrasb@gmail.com>
2023-01-12 22:00:17 -0500

    docker compose file was added

---
+++ src/test/java/org/gutenberg/utilities/StartHub.java
@@ -0,0 +1,17 @@
+package org.gutenberg.grid;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.grid.Main;
+import org.openqa.selenium.net.PortProber;
+
+public class StartHub {
+    public static void main(String[] args) throws Exception {
+        int port = PortProber.findFreePort();
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+
+        Main.main(
+                new String[] { "standalone", "--port", String.valueOf(port) });
+    }
+
+}
